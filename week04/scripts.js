02c36f858a86a0ae487fb86ef0ea4a317b43da38
Jesper <hexeane_451@hotmail.com>
2020-12-04 20:12:28 +0800

    changes "name" to "nama" in script

--- week04/scripts.js
+++ week04/scripts.js
@@ -1,7 +1,7 @@
 function transformUpperCase() {
-    let a = document.getElementById("namePengguna");
+    let a = document.getElementById("namaPengguna");
     a.value = a.value.toUpperCase();
 }
 
-let elUsername = document.getElementById("namePengguna");
+let elUsername = document.getElementById("namaPengguna");
 elUsername.addEventListener("keyup", transformUpperCase);
