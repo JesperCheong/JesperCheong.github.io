cef50b957ea29e0af28a957c988515480dea5fbc
Jesper <hexeane_451@hotmail.com>
2020-12-07 21:15:52 +0800

    fixed sunway&klcc distance decimal error

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -26,8 +26,8 @@ function roundVal(val) {
 
 function calculateDistances(lat,lon) {
     let d1 = haversine(lat, lon, 2.922562, 101.650965); //depulze
-    let d2 = haversine(lat, lon, 3.073065, 101607787); //sunway pyramid
-    let d3 = haversine(lat, lon, 3.158761, 101714524); //klcc
+    let d2 = haversine(lat, lon, 3.073065, 101.607787); //sunway pyramid
+    let d3 = haversine(lat, lon, 3.158761, 101.714524); //klcc
 
     return [d1,d2,d3];
 }
