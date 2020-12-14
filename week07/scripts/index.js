1bb813d90895df740e3bd42f5f3e43716803fbdc
Jesper <hexeane_451@hotmail.com>
2020-12-14 21:14:02 +0800

    v-if

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -1,5 +1,5 @@
 let app = new Vue({
-    el: "#app",
+    el: "#app", // DO NOT MISS THE COMMA 
     data: {
         message: "Hello Vue!"
     }
@@ -9,7 +9,14 @@ let app2 = new Vue({
     el: "#app2",
     data: {
         message: "You loaded this page on " + new Date().toLocaleString(),
-        linkURL: "https://bbc.in/2LAnJs8",
+        linkURL: "https://bbc.in/2LAnJs8", // DO NOT MISS THE COMMA 
         linkText: "Google is down."
     }
-})
+});
+
+let app3 = new Vue({
+    el: "#app3",
+    data: {
+        seen:true
+    }
+});
