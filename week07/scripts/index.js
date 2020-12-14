dccc4bc1268154569f02e6871f731d8164f04afb
Jesper <hexeane_451@hotmail.com>
2020-12-14 20:45:28 +0800

    v-bind

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -3,5 +3,11 @@ let app = new Vue({
     data: {
         message: "Hello Vue!"
     }
-
 });
+
+let app2 = new Vue({
+    el: "#app2",
+    data: {
+        message: "You loaded this page on " + new Date().toLocaleString()
+    }
+})
