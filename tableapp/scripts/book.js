ae9047f80991adc9bfa902529d39f7b5b3fe22e4
Jesper <hexeane_451@hotmail.com>
2020-12-22 20:20:38 +0800

    booking feature

---
+++ tableapp/scripts/book.js
@@ -0,0 +1,40 @@
+function BookNow(guestName, guestEmail, guestPax, guestRemarks) {
+    let url = 'https://api.sheety.co/766f50739fb40a2b659c331650277aa7/bookingApp/bookings';
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            pax: guestPax,
+            remarks: guestRemarks
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            alert(json.booking.name + " added!");
+        });
+}
+/*
+let bookNow = document.getElementById("bookNow");
+bookNow.addEventListener("click", function () {
+    let gName = document.getElementById("guestName").value;
+    let gEmail = document.getElementById("guestEmail").value;
+    let gPax = document.getElementById("guestPax").value;
+
+    BookNow(gName, gEmail, gPax);
+});*/
+
+window.addEventListener("load", function () {
+    document.getElementById("bookNow").addEventListener("click", function () {
+        let userName = document.getElementById("userName").value;
+        let userEmail = document.getElementById("userEmail").value;
+        let userPax = document.getElementById("userPax").value;
+        let userRemarks = document.getElementById("userRemarks").value;
+
+        BookNow(userName, userEmail, userPax, userRemarks)
+    });
+});
